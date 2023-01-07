e29097fbc5242cf7debbe71c609de1807ddf8c09
Naman Sharma <82465837+nxmxn-shxrmx@users.noreply.github.com>
2023-01-07 23:51:49 +0530

    Add files via upload

--- myscripts.js
+++ myscripts.js
@@ -5,6 +5,9 @@ const chess = document.getElementById("chess");
 const content = document.getElementById("content");
 const check = document.getElementById("check");
 const back = document.getElementById("back");
+
+const sleep = ms => new Promise(r => setTimeout(r, ms));
+let boo=0;
 let l = 4;
 let total =0;
 play.addEventListener("click", () => {
@@ -37,6 +40,8 @@ levels.addEventListener("click", (event) => {
 });
 chess.addEventListener("click", (event) => {
     if(event.target.id=="chess")
+    return;
+    if(boo==1)
     return;
     var idL = event.target.id;
     if (idL[idL.length - 1] == "q") idL = idL.slice(0, idL.length - 1);
@@ -71,11 +76,25 @@ check.addEventListener("click",()=>
     }
     if(total==0)
     {
+        boo =1;
         make(arr,0);
+        
     }
     else if(total!=l)
-        alert(total+"Not Fully Filled")
-    
+    {
+       
+        check.style.left="40px";
+        setTimeout(()=>
+        {
+            check.style.left="-60px";
+        },200);
+        setTimeout(()=>
+        {
+            check.style.left="0px";
+
+        },400);
+        
+    }
    else if(f(arr,0))
     {
         alert("correct");
@@ -123,19 +142,38 @@ function correct(arr,i,j)
     }
     return true;
 }
-function correct1(arr,i,j)
+async function changeColor(Block)
+{
+    Block.style.backgroundColor ="red";
+    await sleep(300);
+}
+async function correct1(arr,i,j)
 {
     for(var k =i-1;k>=0;--k)
     {   
         if(arr[k][j]==1)
+        {
+           var Block =document.getElementById(k*arr.length+j);
+           var p =Block.style.backgroundColor;
+           await changeColor(Block)
+
+           Block.style.backgroundColor=p;
             return false;
+        }
     }
     var p = i-1;
     var q = j-1;
     while(p>=0 && q>=0)
     {
         if(arr[p][q]==1)
-        return false;
+        {
+            var Block =document.getElementById(p*arr.length+q);
+            var p =Block.style.backgroundColor;
+            await changeColor(Block)
+
+            Block.style.backgroundColor=p;
+            return false;
+        }
         p--;
         q--;
     }
@@ -144,7 +182,14 @@ function correct1(arr,i,j)
     while(p>=0 && q<arr.length)
     {
         if(arr[p][q]==1)
-        return false;
+        {
+            var Block =document.getElementById(p*arr.length+q);
+            var p =Block.style.backgroundColor;
+            await changeColor(Block)
+
+            Block.style.backgroundColor=p;
+            return false;
+        }
         q++;
         p--;
     }
@@ -159,17 +204,18 @@ function f(arr,i)
     {
         if(arr[i][j]==1)
         {
-            if(!correct(arr,i,j))
+            if(!(correct(arr,i,j)))
                 return false;
         }
     }
     return f(arr,i+1);
 }
-function make(arr,i)
+    async function  make(arr,i)
 {
     if(i==arr.length)
+    { boo=0;
     return true;
-
+    }
     for(var j =0;j<arr.length;++j)
     {
         
@@ -177,10 +223,13 @@ function make(arr,i)
         var Block =document.getElementById(i*arr.length+j);
         total++;
         Block.innerHTML = "<i class='fa-solid fa-chess-queen queen' id=" + i*arr.length+j + "q" + "></i>";
-        if(correct1(arr,i,j))
+        await sleep(300);
+        if(await correct1(arr,i,j))
         {
-            if(make(arr,i+1))
+            if(await make(arr,i+1))
+            {
                 return true;
+            }
             
         }
         total--;
